9a5fa3280f053a2235c88728f639335bac55d0aa
latif.altun <latif.altun@apsiyon.com>
2025-09-29 17:23:28 +0300

    update email utils

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,5 +1,5 @@
-import os
 import resend
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,15 +8,17 @@ RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 resend.api_key = RESEND_API_KEY
 
 FROM_EMAIL = os.getenv("FROM_EMAIL")
+
 def send_email(to_email: str, subject: str, body: str):
     try:
-        params = {
-            "from": FROM_EMAIL,
-            "to": [to_email],
-            "subject": subject,
-            "html": body,
+        params: resend.Emails.SendParams = {
+        "from": FROM_EMAIL,
+        "to": [to_email],
+        "subject": subject,
+        "html": body
         }
         response = resend.Emails.send(params)
         return response
     except Exception as e:
+        print(f"❌ Resend API error: {str(e)}")
         raise Exception(f"Resend API error: {str(e)}")
